c6beded3e5e94b2dde4939a31f05db3f7e76af82
William Hincapié Medina - Ceiba Software <william.hincapie@ceiba.com.co>
2018-06-25 16:04:03 -0500

    Added Jenkinsfile

---
+++ parkinglot-service/src/main/java/co/com/ceiba/parkinglotservice/ParkinglotServiceApplication.java
@@ -0,0 +1,12 @@
+package co.com.ceiba.parkinglotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotServiceApplication.class, args);
+	}
+}
